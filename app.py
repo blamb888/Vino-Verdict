e6767c7621295c3c40d471422dfd3ac1a2489c2c
Sultan Lulecioglu <olsadayok@gmail.com>
2023-08-05 17:05:20 +0900

    needs update after model function

--- app.py
+++ app.py
@@ -11,12 +11,17 @@ st.caption ('any caption here if needed')
 
 st.latex(" x^2 math things here")
 
-
+#model = loadMySuperModel()
 
 st.subheader('INPUT BOX: input some wine label/review #(decide which one)')
 
-st.text_input("Review or label")
+review=st.text_input("Review or label")
+
+st.button("Check if recommended")
+st.write(review) #for now
 
 
 
-st.button ("Check if recommended")
+#if st.button("Check if recommended"):
+#    label = model.predict(review)
+#    st.write(label)
