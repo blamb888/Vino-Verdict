2b8d7691ca790f23c585c295f85c357e28d96a22
Sultan Lulecioglu <olsadayok@gmail.com>
2023-08-05 16:05:40 +0900

    Add Makefile and app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+#first pip install streamlit
+import streamlit as st
+
+st.write('Trying if I can make streamlit work')
+
+st.title('this will be title')
+
+st.subheader('subheader here')
+
+st.caption ('any caption here if needed')
+
+st.latex(" x^2 math things here")
+
+
+
+st.subheader('INPUT BOX: input some wine label/review #(decide which one)')
+
+st.title('put an arrow that will output the ML result')
