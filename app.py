2c279327b5a560fd276500db8d486d4dc697a81b
Sultan Lulecioglu <olsadayok@gmail.com>
2023-08-17 23:36:49 +0900

    added images and some polishing

--- app.py
+++ app.py
@@ -1,57 +1,29 @@
 #first pip install streamlit
 import streamlit as st
-
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
+from PIL import Image
 
 
-#---------------------------------- first draft--------
-st.title('Vino Verdict test page')
-
-
-st.subheader('Recommendation level based on wine reviews -- ??? ')
-
-#st.caption ('any caption here if needed')
-
-#st.latex(" x^2 math things here")
-
-#model = loadMySuperModel()
-
-
-st.subheader('INPUT BOX: input some wine label/review #(to be decided- which one)')
-
-
-
-review=st.text_input("Review or label")
-
-
-
-st.button("Check if recommended")
-st.write(review) #for now
-
-
-
-#if st.button("Check if recommended"):
-#    label = model.predict(review)
-#    st.write(label)
-
-
-#-------------------------------------------------------
 
 # Load model and tokenizer
-# bin file should be named as pytorch_model.bin, and config.json should be in the folder
-MODEL_PATH = "/Users/sultanl/code/blamb888/Vino-Verdict"
+##pretrained model from bert
+##model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
 
+# bin file should be named as pytorch_model.bin, and config.json should be in the project folder
+
+MODEL_PATH = "/Users/sultanl/code/blamb888/Vino-Verdict"
 model = BertForSequenceClassification.from_pretrained(MODEL_PATH)
-#model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 ######## add preprocessing functions here
 
 st.title('Wine Review Predictor')
+vino=Image.open('./members/vino.jpeg')
+st.image(vino)
 
 # Get user input
-review = st.text_area("Enter wine review:")
+review = st.text_area("Enter wine review:", height=7)
 
 if st.button("Predict"):
     # Tokenize and preprocess input
@@ -66,5 +38,30 @@ if st.button("Predict"):
         logits = model(inputs['input_ids'], attention_mask=inputs['attention_mask'])[0]
         prediction = torch.argmax(logits, dim=1).item()
 
-    # Display prediction
-    st.write(f'Predicted Rating Category: {prediction}')
+    # Display prediction  #Predicted Rating Category
+    if prediction==1:
+        st.write('Sentiment Result: Good Wine!')
+    if prediction==0:
+        st.write('Sentiment Result: Bad Wine!')
+    if prediction==2:
+        st.write('Sentiment Result: Average Wine!')
+
+
+
+
+
+#project members
+st.divider()
+st.subheader("Vino-Verdict members")
+col1, col2, col3 = st.columns(3, gap='medium')
+
+
+bl=Image.open('./members/bl.jpg')
+sl=Image.open('./members/sl.jpg')
+ao=Image.open('./members/ao.jpg')
+with col1:
+    st.image(bl,caption='Team leader: Brandon')
+with col2:
+    st.image(sl,caption='Team member: Sultan')
+with col3:
+    st.image(ao,caption='Team member: Ayata')
